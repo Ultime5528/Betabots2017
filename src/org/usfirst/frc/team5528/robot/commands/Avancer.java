2fa018fa8216574f831b2a7a40163941de766c6e
Ultime 5528 <keranna.first@gmail.com>
2017-11-28 18:25:59 -0500

    Erreur setSetpointDistance

--- src/org/usfirst/frc/team5528/robot/commands/Avancer.java
+++ src/org/usfirst/frc/team5528/robot/commands/Avancer.java
@@ -19,7 +19,7 @@ public class Avancer extends Command {
     // Called just before this Command runs the first time
     protected void initialize() {
     	Robot.basePilotable.resetEnc();
-    	Robot.basePilotable.setSetpointAngle(distance);
+    	Robot.basePilotable.setSetpointDistance(distance);
     	Robot.basePilotable.setValeurAngle(0.0);
     	Robot.basePilotable.enableDistance();
     	
